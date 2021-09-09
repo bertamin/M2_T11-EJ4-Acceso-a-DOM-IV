d414363543b65d5d066b55b826484fb411ed06b6
Berta Minguela <bmd1829@hotmail.com>
2021-09-09 16:12:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function insertarFila(){
+    		var z = document.createElement("TD");
+
+    		z.innerHTML= "La celda de la tabla es esta";
+    		
+    		document.getElementById("miTabla").appendChild(z);
+	}
+			
+		
+	function insertarColumna(){
+				var z = document.createElement("TD");
+
+    		z.innerHTML= "Nueva columna";
+
+    		document.getElementById("myRow").appendChild(z);
+			
+		
+    	}
